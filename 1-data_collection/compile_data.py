e645243b8752feaa738513ec518f8d1b789383e2
carolinejjung <cjung8321@gmail.com>
2024-04-28 19:42:41 -0400

    compile all data scraped into one csv file

---
+++ 1-data_collection/compile_data.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import os
+import json
+import re
+
+
+def clean_description(description):
+    """
+    Helper function, takes video description and splits into words, removes punctuation, emojis and stop words.
+    """
+    if pd.isna(description):  
+        return []
+    description = re.sub(r'\d+', '', description) # remove numbers from the text
+    words = description.split() # split the description into words
+    cleaned_words = [re.sub(r'[^\w\s]', '', word).lower() for word in words] # remove punctuation and emojis, make everything lowercase
+    cleaned_words = [word for word in cleaned_words if word] # remove empty strings
+    return cleaned_words
+
+def combine_data(gender):
+    if gender=="M":
+        filepath = "output_male/"
+        name = "male"
+    elif gender=="F":
+        filepath = "output_female/"
+        name = "female"
+    else:
+        print("Not a valid input.")
+    files = os.listdir(filepath)
+
+    all_description, all_ids, all_comments = [], [], []
+    for file in files:
+        with open(filepath + file, 'r') as f:
+            account = json.load(f)
+        f.close()
+
+        for video in account:
+            vid_desc = ""
+            try: 
+                for parts in video["description"]:
+                    vid_desc += parts.lower()
+                all_description.append(vid_desc)
+                all_ids.append(video["id"])
+                all_comments.append(video["comments"])
+            except:
+                pass
+
+    cleaned = [clean_description(desc) for desc in all_description]
+    data = pd.DataFrame()
+    data["video_id"] = all_ids
+    data["description_string"] = all_description
+    data["description_list"] = cleaned
+    data["comments"] = all_comments
+
+    data.to_csv(f"compiled_data/{name}.csv")
+    return data
+
+combine_data("M")
+combine_data("F")
